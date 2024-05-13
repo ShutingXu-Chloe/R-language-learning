f85fa9c3bebf07e1a7ee9c3ce8cd816d4295324c
ShutingXu-Chloe <163435015+ShutingXu-Chloe@users.noreply.github.com>
2024-05-14 01:54:29 +0800

    Update ISL_2.4.8.R

--- ISL_2.4.8.R
+++ ISL_2.4.8.R
@@ -6,7 +6,7 @@ dim(College)
 # a part of data. Thus we use function 'rownames()'
 # However, an error occurs.Error in `.rowNamesDF<-`(x, value = value) : 
 # 不允许有重复的'row.names.
-# We can transpose the original data set twice to obtain a matrix 'college'
+# We can transpose the original data set 
 
 rownames(College)=colnames(t(College))
 rownames(College)
